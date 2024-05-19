454f9e06ee1080385dbc69824710a3abf02e567b
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-05-19 16:17:25 +0500

    server side validation for creating invoice and editing an invoice

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
  
 export default async function Page({ params }: { params: { id: string } }) {
@@ -9,6 +10,9 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+   if (!invoice) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
@@ -26,7 +30,3 @@ export default async function Page({ params }: { params: { id: string } }) {
   );
 }
 
-export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
-}
