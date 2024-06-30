9370133b91fde7164d38c6bb52cea7af9f51d39a
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-06-30 18:37:07 +0500

    tried and tested all variables in PHP

--- PHP tutorial/index copy.php
+++ PHP tutorial/index copy.php
@@ -1,6 +1,66 @@
 <?php
-echo "i love pizza <br>";
-echo "its really good <br>";
+//Echoing a message"
+// echo "i love pizza <br>";
+// echo "its really good <br>";
+
+//variables
+// // Integer variable
+// $age = 25; // Age in years
+
+// // Float variable
+// $height = 5.9; // Height in feet
+
+// // String variable
+// $name = "John Doe"; // Full name
+
+// // Boolean variable
+// $isStudent = true; // Is the person a student?
+
+// // Array variable
+// $colors = array("red", "green", "blue"); // Array of favorite colors
+
+// // Associative array
+// $person = array("first_name" => "John", "last_name" => "Doe", "age" => 25); // Person details
+
+// // Null variable
+// $middleName = null; // Middle name (not provided)
+
+// // Object variable
+// class Car
+// {
+//     public $make;
+//     public $model;
+//     public function __construct($make, $model)
+//     {
+//         $this->make = $make;
+//         $this->model = $model;
+//     }
+// }
+// $myCar = new Car("Toyota", "Corolla"); // Car object
+
+// // Resource variable (example with a database connection)
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "test_db";
+// $conn = new mysqli($server, $username, $password, $dbname); // Database connection
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// // Printing variables
+// echo "Age: $age\n";
+// echo "Height: $height\n";
+// echo "Name: $name\n";
+// echo "Is Student: " . ($isStudent ? "Yes" : "No") . "\n";
+// echo "Favorite Colors: " . implode(", ", $colors) . "\n";
+// echo "Person Details: " . $person['first_name'] . " " . $person['last_name'] . ", Age: " . $person['age'] . "\n";
+// echo "Middle Name: " . ($middleName ?? "Not provided") . "\n";
+// echo "Car: " . $myCar->make . " " . $myCar->model . "\n";
+
+// // Closing the database connection
+// $conn->close();
 ?>
 
 <!DOCTYPE html>
