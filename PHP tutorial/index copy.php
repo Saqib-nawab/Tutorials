2c0fc3c0e000b2dd8b4671068f1d2cb6e212f74c
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-06-30 18:25:06 +0500

    echoed a message in PHP and ran HTML code in PHP

---
+++ PHP tutorial/index copy.php
@@ -0,0 +1,19 @@
+<?php
+echo "i love pizza <br>";
+echo "its really good <br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Adding HTML code to a PHP file</p>
+</body>
+
+</html>
