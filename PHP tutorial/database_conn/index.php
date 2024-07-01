d49a9f0732c68861247d196b011b519f0b72a006
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-07-01 18:59:54 +0500

    SQL connetion established

---
+++ PHP tutorial/database_conn/index.php
@@ -0,0 +1,24 @@
+<?php
+include("header.html");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Adding HTML code to a PHP file</p>
+</body>
+
+</html>
+
+<?php
+include("footer.html");
+
+?>
