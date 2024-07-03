ac17333de9771bfdfbf39e6ffb71d82680243bc8
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-07-03 16:05:31 +0500

    find jobs model

--- laravel/example/routes/web.php
+++ laravel/example/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -11,53 +12,14 @@ Route::get('/', function () {
 Route::get('/jobs', function () {
     return view(
         'jobs',
-        [
-            'jobs' => [
-                [
-                    'id' => '1',
-                    'title' => 'Director',
-                    'salary' => '$10,000',
-                ],
-                [
-                    'id' => '2',
-                    'title' => 'Developer',
-                    'salary' => '$5,000',
-                ],
-                [
-                    'id' => '3',
-                    'title' => 'Designer',
-                    'salary' => '$3,000',
-                ]
-            ]
-        ]
-
-
-    );
+        ['jobs' => Job::all()]
+    );;
 });
 
 
 //new route for getting inteded job
 Route::get('/jobs/{id}', function ($id) {
-
-    $jobs = [
-        [
-            'id' => '1',
-            'title' => 'Director',
-            'salary' => '$10,000',
-        ],
-        [
-            'id' => '2',
-            'title' => 'Developer',
-            'salary' => '$5,000',
-        ],
-        [
-            'id' => '3',
-            'title' => 'Designer',
-            'salary' => '$3,000',
-        ]
-    ];
-
-    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id); //searching the job with intended id
+    $job = Job::find($id);
     // dd($job);
     return View('job', ['job' => $job]);
 });
