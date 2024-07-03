2da1ad2dc96180330bc458cdb9993fdf7ec9f180
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-07-03 19:59:52 +0500

    fake factories created for Users and Jobs

--- laravel/example/app/Models/Job.php
+++ laravel/example/app/Models/Job.php
@@ -2,34 +2,47 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job
+
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return
-            [
-                [
-                    'id' => '1',
-                    'title' => 'Director',
-                    'salary' => '$10,000',
-                ],
-                [
-                    'id' => '2',
-                    'title' => 'Developer',
-                    'salary' => '$5,000',
-                ],
-                [
-                    'id' => '3',
-                    'title' => 'Designer',
-                    'salary' => '$3,000',
-                ]
-            ];
-    }
 
-    public static function find($id): array
-    {
-        return Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-    }
+    use HasFactory;
+    protected $table = 'job_listing';
+
+    protected $fillable = ['title', 'salary'];
 }
+
+
+// without model
+// class Job
+// {
+//     public static function all(): array
+//     {
+//         return
+//             [
+//                 [
+//                     'id' => '1',
+//                     'title' => 'Director',
+//                     'salary' => '$10,000',
+//                 ],
+//                 [
+//                     'id' => '2',
+//                     'title' => 'Developer',
+//                     'salary' => '$5,000',
+//                 ],
+//                 [
+//                     'id' => '3',
+//                     'title' => 'Designer',
+//                     'salary' => '$3,000',
+//                 ]
+//             ];
+//     }
+
+//     public static function find($id): array
+//     {
+//         return Arr::first(static::all(), fn ($job) => $job['id'] == $id);
+//     }
+// }
