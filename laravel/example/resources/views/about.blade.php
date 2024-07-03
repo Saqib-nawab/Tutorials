d1f94b45a1ea83b420030541cfab5c89df5a0ddb
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-07-03 11:22:31 +0500

    tailwind navbar added

--- laravel/example/resources/views/about.blade.php
+++ laravel/example/resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        About page
+    </x-slot:heading>
     <p>Hello, About</p>
 </x-layout>
--- laravel/example/resources/views/home.blade.php
+++ laravel/example/resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        About page
+    </x-slot:heading>
     <p>Hello, Home</p>
 </x-layout>
